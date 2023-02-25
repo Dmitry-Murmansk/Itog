4a88c0f25625eebb87550f51ebbd9334589d7e32
Dmitry-Murmansk <119297978+Dmitry-Murmansk@users.noreply.github.com>
2023-02-25 04:45:14 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,67 +1,46 @@
-﻿using System;
-					
-public class Program
-{
-	public static void Main()
-	{
-		// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
-		// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
-		// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
-		// лучше обойтись исключительно массивами.
-		// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
-		// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
-		// [“Russia”, “Denmark”, “Kazan”] → []
+﻿string[] array1 = { "hello", "2", "world", ":-)", 
+"123" , "1234", "1567", "-2", "computer science", 
+"Russia", "Denmark", "Kazan", "FCK"}; // Задаем наш массив
+int length = 0; // Обнуляем счетчик элементов, длина которых <= 3 символам
 
-        string[] arr1 = {"Hello", "2", "world", ":-)"};
-		string[] arr2 = {"1234", "1567", "-2", "computer science"};
-		string[] arr3 = {"Russia", "Denmark", "Kazan"};
-		
-		int MAX_WORD_LENGTH = 3;
-		
-		string[] GetShortWordsArray(string[] array)
-        {
-			int length = array.Length;			
-			string[] result = new string[length];
-			int count = 0;
-			
-			for (int i = 0; i < length; i++)
-			{
-				if(array[i].Length <= MAX_WORD_LENGTH)
-				{
-					result[count] = array[i];
-					count++;
-				}
-			}
-            Array.Resize(ref result, count);
-			
-			return result;
-		}
-		
-		void PrintArray(string[] array)
-		{
-			if(array.Length == 0)
-			{
-				Console.WriteLine("Array is empty");
-			}
-			else
-			{
-				Array.ForEach(array, (str) => Console.Write($"{str} "));
-				Console.WriteLine();
-			}
-		}
-		
-		void PrintTask(string[] array)
-		{
-			Console.WriteLine("Source array:");
-			PrintArray(array);
-			string[] shortArray = GetShortWordsArray(array);
-			Console.WriteLine($"Result array (words are less or equal than {MAX_WORD_LENGTH} symbols)");
-			PrintArray(shortArray);
-			Console.WriteLine();
-		}
-		
-		PrintTask(arr1);
-		PrintTask(arr2);
-		PrintTask(arr3);
-	}
+int ArrayElementsCountLessThanThree(string[] array) // Считаем количество элементов массива,  длина которых <= 3 символам
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) length++;
+    }
+    return length;
 }
+
+void ArrayElementsWriteOut(string[] array1,string[] array2) //Копируем элементы  <= 3 символам в новый массив
+{
+    int i = 0;
+    int j = 0;
+    while(i < array1.Length)
+    {
+        if (array1[i].Length <= 3) 
+        {
+            array2[j] = array1[i];
+            j++;
+        }
+        i++;
+    }
+}
+
+void ArrayElementsOutput(string[] array) // Вывод массива
+{
+    for (int i = 0; i < array.Length; i++) System.Console.Write($"{array[i]} ");
+}
+
+ArrayElementsCountLessThanThree(array1); // Считаем длину нового массива (получаем length)
+string[] array2 = new string[length]; // Создаем новый массив длиной length
+ArrayElementsWriteOut(array1, array2); // Заполняем новый массив элементами  <= 3 символам
+ArrayElementsOutput(array1); // Выводим изначальный массив
+Console.WriteLine(" ");
+ArrayElementsOutput(array2); // Выводим новый массив с элементами <= 3 символам
+if (length == 0) Console.WriteLine("В заданном массиве нет элементов <= 3 символам");
+
+// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
